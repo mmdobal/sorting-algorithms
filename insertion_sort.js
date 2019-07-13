28821a86b4990325a29dc6126251a7e375dda2c9
Marianna <mddobal@gmail.com>
2019-07-13 11:41:26 -0300

    insertion sort done

---
+++ insertion_sort.js
@@ -0,0 +1,13 @@
+
+function insertion_sort (array) {
+    for (let j = 1; j < array.length; j += 1){
+        let key = array[j];
+        let i = j - 1;
+        while (i >= 0 && array[i] > key){
+            array[i + 1] = array[i];
+            i = i - 1;
+        }
+        array[i + 1] = key;
+    }
+    return array;
+}
